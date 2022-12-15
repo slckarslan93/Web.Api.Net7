92ddeca3c5bc3a12bcdd821c5a7e23b47fa5dfbe
Selcuk ARSLAN <slckarslan93@gmail.com>
2022-12-15 11:36:07 +0300

    Character api controller added

---
+++ ApiProject/Controllers/CharacterController.cs
@@ -0,0 +1,19 @@
+﻿using ApiProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProject.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController:ControllerBase
+    {
+        private static Character knight = new Character();
+
+        [HttpGet]
+        public ActionResult<Character> Get()
+        {
+            return Ok(knight);
+        }
+
+    }
+}
---
+++ ApiProject/Models/RpgClass.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ApiProject.Models
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum RpgClass
+    {
+        Knight = 1,
+        Mage,
+        Cleric
+
+    }
+}
